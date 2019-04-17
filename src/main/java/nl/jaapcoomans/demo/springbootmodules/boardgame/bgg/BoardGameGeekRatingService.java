e2ffb2f47718061c1614b172369d773e030a3bed
Jaap Coomans <jaapcoomans@gmail.com>
2019-04-17 23:02:51 +0200

    Initial version of BoardGameGeek rating service

---
+++ src/main/java/nl/jaapcoomans/demo/springbootmodules/boardgame/bgg/BoardGameGeekRatingService.java
@@ -0,0 +1,62 @@
+package nl.jaapcoomans.demo.springbootmodules.boardgame.bgg;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.stream.Stream;
+
+import nl.jaapcoomans.demo.springbootmodules.boardgame.bgg.xmlapi.BigDecimalValue;
+import nl.jaapcoomans.demo.springbootmodules.boardgame.bgg.xmlapi.ItemWithStatistics;
+import nl.jaapcoomans.demo.springbootmodules.boardgame.bgg.xmlapi.Items;
+import nl.jaapcoomans.demo.springbootmodules.boardgame.bgg.xmlapi.Statistics;
+import nl.jaapcoomans.demo.springbootmodules.boardgame.bgg.xmlapi.Statistics.Ratings;
+import nl.jaapcoomans.demo.springbootmodules.boardgame.domain.GameRatingService;
+
+import feign.Feign;
+import feign.Logger.ErrorLogger;
+import feign.Logger.Level;
+import feign.jaxb.JAXBContextFactory;
+import feign.jaxb.JAXBDecoder;
+import feign.jaxb.JAXBEncoder;
+
+public class BoardGameGeekRatingService implements GameRatingService {
+	private BoardGameGeekClient bggClient;
+
+	public BoardGameGeekRatingService(final String baseUrl) {
+		this.bggClient = createClient(baseUrl);
+	}
+
+	private BoardGameGeekClient createClient(String baseUrl) {
+		JAXBContextFactory jaxbFactory = new JAXBContextFactory.Builder()
+			.withMarshallerJAXBEncoding("UTF-8")
+			.build();
+
+		return Feign.builder()
+			.encoder(new JAXBEncoder(jaxbFactory))
+			.decoder(new JAXBDecoder(jaxbFactory))
+			.logger(new ErrorLogger())
+			.logLevel(Level.BASIC)
+			.target(BoardGameGeekClient.class, baseUrl);
+	}
+
+	@Override
+	public Optional<BigDecimal> getAverageRating(final UUID gameId) {
+		int bggId = this.resolveBggId(gameId);
+		Optional<Items> items = Optional.ofNullable(this.bggClient.getItemsWithRating(bggId));
+
+		return items
+			.map(Items::getItems)
+			.map(List::stream)
+			.flatMap(Stream::findFirst)
+			.map(ItemWithStatistics::getStatistics)
+			.map(Statistics::getRatings)
+			.map(Ratings::getAverage)
+			.map(BigDecimalValue::getValue);
+	}
+
+	private int resolveBggId(UUID gameId) {
+		//TODO: Implement mapping properly
+		return 0;
+	}
+}
