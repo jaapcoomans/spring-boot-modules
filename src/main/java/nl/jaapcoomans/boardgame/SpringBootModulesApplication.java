5732bf740c4e6096c5aa87dbfa4bb0a022a8e92b
Jaap Coomans <jaapcoomans@gmail.com>
2019-04-29 22:52:04 +0200

    Return application class to root package

--- src/main/java/nl/jaapcoomans/boardgame/SpringBootModulesApplication.java
+++ src/main/java/nl/jaapcoomans/boardgame/SpringBootModulesApplication.java
@@ -1,4 +1,4 @@
-package nl.jaapcoomans.boardgame.application;
+package nl.jaapcoomans.boardgame;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
