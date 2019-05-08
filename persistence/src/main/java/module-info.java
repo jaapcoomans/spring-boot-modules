8461735eafb9d7270965a2d14f703a3dd4446796
Jaap Coomans <jaapcoomans@gmail.com>
2019-05-08 20:52:19 +0200

    Add descriptor for application module and add missing opens and requires clauses as a result of running on the module-path.
    (cherry picked from commit f68cccfee917ac18fd21ba1a43e7ac6d647da928)

--- persistence/src/main/java/module-info.java
+++ persistence/src/main/java/module-info.java
@@ -2,8 +2,13 @@ module nl.jaapcoomans.boardgame.persistence {
 	requires transitive nl.jaapcoomans.boardgame.domain;
 
 	requires transitive java.persistence;
+	requires net.bytebuddy;
 	requires spring.data.jpa;
 	requires spring.tx;
 
+	requires jdk.unsupported;
+
 	exports nl.jaapcoomans.boardgame.persist;
+
+	opens nl.jaapcoomans.boardgame.persist to org.hibernate.orm.core, spring.core;
 }
