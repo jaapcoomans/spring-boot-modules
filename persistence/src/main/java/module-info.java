b1b01001b3c3e71c62378d3d849f24dc95e3fec8
Jaap Coomans <jaapcoomans@gmail.com>
2020-07-09 20:54:00 +0200

    Add required modules and opens statements to make the application boot correctly on the module path.

--- persistence/src/main/java/module-info.java
+++ persistence/src/main/java/module-info.java
@@ -2,8 +2,14 @@ module nl.jaapcoomans.boardgame.persistence {
 	requires transitive nl.jaapcoomans.boardgame.domain;
 
 	requires transitive java.persistence;
+	requires com.fasterxml.classmate;
+	requires net.bytebuddy;
 	requires spring.data.jpa;
 	requires spring.tx;
 
+	requires jdk.unsupported;
+
 	exports nl.jaapcoomans.boardgame.persist;
+
+	opens nl.jaapcoomans.boardgame.persist to org.hibernate.orm.core, spring.core;
 }
