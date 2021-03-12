265c9702237f00d7db303c79c95b11b2bcd73ae2
Jaap Coomans <jaapcoomans@gmail.com>
2021-03-12 21:12:44 +0100

    Add module description for security

---
+++ security/src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module nl.jaapcoomans.boardgame.security {
+    requires com.fasterxml.jackson.core;
+    requires com.fasterxml.jackson.databind;
+    requires org.apache.tomcat.embed.core;
+    requires spring.beans;
+    requires spring.context;
+    requires spring.security.core;
+    requires spring.security.config;
+    requires spring.security.web;
+    requires spring.web;
+
+    requires jjwt.api;
+
+    exports nl.jaapcoomans.boardgame.security;
+    exports nl.jaapcoomans.boardgame.security.rest;
+}
