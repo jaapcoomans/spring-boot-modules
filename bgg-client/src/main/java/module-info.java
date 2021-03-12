b26b09ab48efeacd82e46b2f548ce14f853b46ad
Jaap Coomans <jaapcoomans@gmail.com>
2021-03-12 21:23:24 +0100

    Add module descriptor for bgg-client and replace JAX-B implementation

---
+++ bgg-client/src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module nl.jaapcoomans.boardgame.bgg {
+	requires nl.jaapcoomans.boardgame.domain;
+
+	requires java.xml.bind;
+
+	requires feign.core;
+	requires feign.jaxb;
+
+	exports nl.jaapcoomans.boardgame.bgg.factory;
+
+	opens nl.jaapcoomans.boardgame.bgg.xmlapi to java.xml.bind;
+}
